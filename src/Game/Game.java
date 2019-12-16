60c594c42dfb20a500f130fc2edd49720f203119
Nouani <nouanigabriel@hotmail.com>
2019-12-16 14:31:00 -0300

    implementação da thread

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -12,6 +12,9 @@ public class Game extends Canvas implements Runnable{
 	public static final int HEIGHT = 120;
 	public static final int SCALE = 3;
 	
+	private Thread thread;
+	private boolean isRunnable;
+	
 	public Game() {
 		this.setPreferredSize(new Dimension(Game.WIDTH*Game.SCALE,Game.HEIGHT*Game.SCALE));
 		initFrame();
@@ -27,6 +30,24 @@ public class Game extends Canvas implements Runnable{
 		this.frame.setVisible(true);
 	}
 	
+	public synchronized void start() {
+		this.isRunnable = true;
+		this.thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.start();
@@ -34,7 +55,31 @@ public class Game extends Canvas implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		double lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		
+		while (isRunnable) {
+			double now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: "+frames);
+				frames = 0;
+				timer += 1000;
+			}
+		}
 		
 	}
 
